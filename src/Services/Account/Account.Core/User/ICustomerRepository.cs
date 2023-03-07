6324ab30d58c3139617f6d4c4cab5514d6ce9bb6
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-07 16:06:49 +0100

    Add the customer repository interface

---
+++ src/Services/Account/Account.Core/User/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Account.Core.User;
+
+public interface ICustomerRepository
+{
+    void Create(Customer customer);
+    Customer GetByEmail(string email);
+    void Update(Customer customer);
+    void Delete(Guid id);
+}
