c939049b53db9d4fe0a553943573204d02a6fcb4
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-21 13:40:52 +0100

    add infrastructure project - add Repository implementation

---
+++ src/Services/Inventory/Inventory.Infrastructure/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Inventory.Core.Product;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Infrastructure;
+
+public class DataContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        // necessary to save DateTime
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+    }
+    
+}
---
+++ src/Services/Inventory/Inventory.Infrastructure/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using Inventory.Core.Product;
+
+namespace Inventory.Infrastructure.Repository;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly DataContext _context;
+    
+    public ProductRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public Product GetById(Guid id)
+    {
+        return _context.Products.Find(id);
+    }
+
+    public void Create(Product entity)
+    {
+        _context.Products.Add(entity);
+        _context.SaveChanges();
+    }
+
+    public void Update(Product entity)
+    {
+        _context.Products.Update(entity);
+        _context.SaveChanges();
+    }
+
+    public void Delete(Guid id)
+    {
+        var product = GetById(id);
+        _context.Products.Remove(product);
+        _context.SaveChanges();
+    }
+
+    public ICollection<Product> Search(string search)
+    {
+        return _context.Products.Where(p => p.Name.Contains(search) || p.Description.Contains(search)).ToList();
+    }
+}
