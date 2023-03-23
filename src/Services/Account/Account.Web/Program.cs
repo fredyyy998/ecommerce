e95c1f9b7724387373817b7b0f31612a0a191648
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-23 13:45:26 +0100

    Revert "add configuration installer to simplify Program.cs"
    This reverts commit 600b0ae3

--- src/Services/Account/Account.Web/Program.cs
+++ src/Services/Account/Account.Web/Program.cs
@@ -1,4 +1,19 @@
-using Ecommerce.Common.Web;
+using System.Reflection;
+using System.Text;
+using Account.Application;
+using Account.Application.Dtos;
+using Account.Application.EventHandler;
+using Account.Application.Profile;
+using Account.Core.Events;
+using Account.Core.User;
+using Account.Infrastructure;
+using Account.Infrastructure.MessageBus;
+using Account.Infrastructure.Repository;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 
@@ -22,8 +37,39 @@ var builder = WebApplication.CreateBuilder(args);
 
         options.OperationFilter<SecurityRequirementsOperationFilter>();
     });
+
+    builder.Services.AddScoped<IValidator<CustomerCreateDto>, CustomerCreateDtoValidator>();
+    builder.Services.AddAutoMapper(typeof(MappingProfile));
+    builder.Services.AddDbContext<DataContext>(options =>
+        options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+            b => b.MigrationsAssembly("Account.Web")));
     
-    builder.Services.InstallServices(builder.Configuration, typeof(IServiceInstaller).Assembly);
+
+    var jwtInformation = new JwtInformation(configuration["JWT:Secret"], configuration["JWT:ValidIssuer"], configuration["JWT:ValidAudience"]);
+    builder.Services.AddSingleton<JwtInformation>(jwtInformation);
+    
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT:Secret").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+    
+    builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+    builder.Services.AddScoped<IProfileService, ProfileService>();
+
+    builder.Services.AddScoped<IMessageBus, KafkaProducer>();
+    builder.Services.AddScoped<INotificationHandler<CustomerRegisteredEvent>, CustomerRegisteredEventHandler>();
+    builder.Services.AddScoped<INotificationHandler<CustomerEditedEvent>, CustomerEditedEventHandler>();
+    
+    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 }
 
 var app = builder.Build();
