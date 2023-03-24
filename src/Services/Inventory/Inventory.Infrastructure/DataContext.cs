add7a70acc568e0684fafb4c833ea5cedc985d89
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-24 10:04:10 +0100

    add functionallity to data context that automatically publishes the events from the saved domain to the mediator

--- src/Services/Inventory/Inventory.Infrastructure/DataContext.cs
+++ src/Services/Inventory/Inventory.Infrastructure/DataContext.cs
@@ -1,4 +1,6 @@
-﻿using Inventory.Core.Product;
+﻿using Ecommerce.Common.Core;
+using Inventory.Core.Product;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Inventory.Infrastructure;
@@ -6,9 +8,12 @@ namespace Inventory.Infrastructure;
 public class DataContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
+
+    private IMediator _mediator;
     
-    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    public DataContext(DbContextOptions<DataContext> options, IMediator mediator) : base(options)
     {
+        _mediator = mediator;
         // necessary to save DateTime
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
     }
@@ -21,5 +26,23 @@ public class DataContext : DbContext
             entity.OwnsMany(p => p.Information);
         });
     }
-    
+
+    public override int SaveChanges()
+    {
+        // first save the changes to the repo, then publish so the published data is always persisted correctly
+        var result=  base.SaveChanges();
+        
+        var domainEntities = this.ChangeTracker
+            .Entries<EntityRoot>()
+            .Where(x => x.Entity.DomainEvents != null && x.Entity.DomainEvents.Any());
+
+        var domainEvents = domainEntities
+            .SelectMany(x => x.Entity.DomainEvents)
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+            _mediator.Publish(domainEvent);
+
+        return result;
+    }
 }
