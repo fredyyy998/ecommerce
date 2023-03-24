b7e65c32ca75dd8005f75209fcc376e1d1ce8631
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-24 10:01:44 +0100

    add ProductUpdatedByAdmin.cs event

---
+++ src/Services/Inventory/Inventory.Core/DomainEvents/ProductUpdatedByAdmin.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Common.Core;
+
+namespace Inventory.Core.DomainEvents;
+
+public class ProductUpdatedByAdmin : IDomainEvent
+{
+    public Product.Product Product { get; }
+    
+    public ProductUpdatedByAdmin(Product.Product product)
+    {
+        Product = product;
+    }
+}
