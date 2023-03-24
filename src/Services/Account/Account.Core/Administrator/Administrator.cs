2a43a380531ea5d1a697903eed7d5627f9564637
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-24 10:42:01 +0100

    add Administrator domain class

---
+++ src/Services/Account/Account.Core/Administrator/Administrator.cs
@@ -0,0 +1,20 @@
+﻿using Account.Core.User;
+
+namespace Account.Core.Administrator;
+
+public class Administrator : User.User
+{
+    public string Role { get; protected set; }
+    
+    public static Administrator Create(string email, string password)
+    {
+        return new Administrator()
+        {
+            Id = Guid.NewGuid(),
+            Email = email,
+            Password = Password.Create(password),
+            Role = "Admin",
+            CreatedAt = DateTime.Now,
+        };
+    }
+}
