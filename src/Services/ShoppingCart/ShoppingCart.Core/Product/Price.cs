1953efc2ac04ad18f6f2c813be01eb009695603a
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-27 16:09:50 +0200

    Add basic product structure to shopping cart service

---
+++ src/Services/ShoppingCart/ShoppingCart.Core/Product/Price.cs
@@ -0,0 +1,27 @@
+﻿using Ecommerce.Common.Core;
+
+namespace ShoppingCart.Core.Product;
+
+public class Price : ValueObject
+{
+    public decimal NetPrice { get; private set; }
+    
+    public decimal GrossPrice { get; private set; }
+    
+    public string CurrencyCode { get; private set; }
+    
+    
+    public Price(decimal netPrice, decimal grossPrice, string currencyCode)
+    {
+        NetPrice = netPrice;
+        GrossPrice = grossPrice;
+        CurrencyCode = currencyCode;
+    }
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return NetPrice;
+        yield return GrossPrice;
+        yield return CurrencyCode;
+    }
+}
---
+++ src/Services/ShoppingCart/ShoppingCart.Core/Product/Product.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Common.Core;
+
+namespace ShoppingCart.Core.Product;
+
+public class Product : EntityRoot
+{
+    public string Name { get; private set; }
+    
+    public string Description { get; private set; }
+    
+    public Price Price { get; private set; }
+    
+    public int Stock { get; private set; }
+    
+    public Product(Guid id, string name, string description, Price price, int stock)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Price = price;
+        Stock = stock;
+    }
+    
+    
+}
