024c905b1ff25caf4ae43ddc01003aac45bbc218
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-28 17:28:53 +0200

    add ShoppingCart Domain Model to ShoppingCart Service

---
+++ src/Services/ShoppingCart/ShoppingCart.Core/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,57 @@
+﻿using Ecommerce.Common.Core;
+using ShoppingCart.Core.Exceptions;
+
+namespace ShoppingCart.Core.ShoppingCart;
+
+public class ShoppingCart : EntityRoot
+{
+    public Guid CustomerId { get; private set; }
+
+    private List<ShoppingCartItem> _items;
+    
+    public IReadOnlyCollection<ShoppingCartItem> Items => _items.AsReadOnly();
+    
+    public DateTime CreatedAt { get; private set; }
+    
+    public DateTime? UpdatedAt { get; private set; }
+    
+    private ShoppingCart(Guid customerId)
+    {
+        Id = Guid.NewGuid();
+        CustomerId = customerId;
+        _items = new List<ShoppingCartItem>();
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = null;
+    }
+    
+    public static ShoppingCart Create(Guid customerId)
+    {
+        return new ShoppingCart(customerId);
+    }
+
+    public void AddItem(Product.Product product, int quantity)
+    {
+        var item = _items.FirstOrDefault(x => x.Product.Id == product.Id);
+        if (item != null)
+        {
+            item.IncreaseQuantity(quantity);
+            return;
+        }
+        item = ShoppingCartItem.Create(product, quantity);
+        _items.Add(item);
+    }
+
+    public void RemoveQuantityOfProduct(Product.Product product, int quantity)
+    {
+        var item = _items.FirstOrDefault(x => x.Product.Id == product.Id);
+        if (item == null)
+        {
+            throw new ShoppingCartDomainException("Product not found in shopping cart.");
+        }
+        item.DecreaseQuantity(quantity);
+        if (item.Quantity == 0)
+        {
+            _items.Remove(item);
+        }
+    }
+}
---
+++ src/Services/ShoppingCart/ShoppingCart.Test/ShoppingCart/ShoppingCartTest.cs
@@ -0,0 +1,68 @@
+﻿using ShoppingCart.Core.Exceptions;
+using ShoppingCart.Core.Product;
+
+namespace ShoppingCart.Test.ShoppingCart;
+
+public class ShoppingCartTest
+{
+    [Fact]
+    public void New_Added_Item_Should_Create_New_ShoppingCartItem()
+    {
+        var shoppingCart = Core.ShoppingCart.ShoppingCart.Create(Guid.NewGuid());
+        var product = new Product(Guid.NewGuid(), "Test", "Test", new Price(10, 10, "EUR"), 10);
+        var quantity = 1;
+        
+        shoppingCart.AddItem(product, quantity);
+        
+        Assert.Single(shoppingCart.Items);
+        Assert.Equal(product.Id, shoppingCart.Items.First().Product.Id);
+    }
+    
+    [Fact]
+    public void Existing_Item_Should_Increase_Quantity()
+    {
+        var shoppingCart = Core.ShoppingCart.ShoppingCart.Create(Guid.NewGuid());
+        var product = new Product(Guid.NewGuid(), "Test", "Test", new Price(10, 10, "EUR"), 10);
+        var quantity = 1;
+        
+        shoppingCart.AddItem(product, quantity);
+        shoppingCart.AddItem(product, quantity);
+        
+        Assert.Single(shoppingCart.Items);
+        Assert.Equal(2, shoppingCart.Items.First().Quantity);
+    }
+    
+    [Fact]
+    public void Existing_Item_Should_Decrease_Quantity()
+    {
+        var shoppingCart = Core.ShoppingCart.ShoppingCart.Create(Guid.NewGuid());
+        var product = new Product(Guid.NewGuid(), "Test", "Test", new Price(10, 10, "EUR"), 10);
+        shoppingCart.AddItem(product, 5);
+        
+        shoppingCart.RemoveQuantityOfProduct(product, 1);
+        
+        Assert.Single(shoppingCart.Items);
+        Assert.Equal(4, shoppingCart.Items.First().Quantity);
+    }
+    
+    [Fact]
+    public void Existing_Item_Should_Remove_When_Quantity_Is_Zero()
+    {
+        var shoppingCart = Core.ShoppingCart.ShoppingCart.Create(Guid.NewGuid());
+        var product = new Product(Guid.NewGuid(), "Test", "Test", new Price(10, 10, "EUR"), 10);
+        shoppingCart.AddItem(product, 5);
+        
+        shoppingCart.RemoveQuantityOfProduct(product, 5);
+        
+        Assert.Empty(shoppingCart.Items);
+    }
+    
+    [Fact]
+    public void NotExisting_Item_Should_Throw_Exception()
+    {
+        var shoppingCart = Core.ShoppingCart.ShoppingCart.Create(Guid.NewGuid());
+        var product = new Product(Guid.NewGuid(), "Test", "Test", new Price(10, 10, "EUR"), 10);
+        
+        Assert.Throws<ShoppingCartDomainException>(() => shoppingCart.RemoveQuantityOfProduct(product, 1));
+    }
+}
