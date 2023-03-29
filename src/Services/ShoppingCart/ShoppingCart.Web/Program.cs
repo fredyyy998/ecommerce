3649f0f41d54b617ac77057d3419a3eade0bd53b
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-29 11:17:41 +0200

    adjusted web configuration - added services

--- src/Services/ShoppingCart/ShoppingCart.Web/Program.cs
+++ src/Services/ShoppingCart/ShoppingCart.Web/Program.cs
@@ -1,6 +1,40 @@
+
+using ShoppingCart.Application.Services;
+using ShoppingCart.Application.Utils;
+using ShoppingCart.Core.ShoppingCart;
+
 var builder = WebApplication.CreateBuilder(args);
+{
+    ConfigurationManager configuration = builder.Configuration;
+    
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddHttpContextAccessor();
+
+
+
+    builder.Services.AddAutoMapper(typeof(MappingProfile));    
+    builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
+}
+
 var app = builder.Build();
+{
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
 
-app.MapGet("/", () => "Hello World!");
+    }
 
-app.Run();
+    app.UseCors("corsapp");
+    app.UseExceptionHandler("/error");
+    app.UseHttpsRedirection();
+    app.MapControllers();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+    
+    app.Run();
+}
+
+public partial class Program { }
