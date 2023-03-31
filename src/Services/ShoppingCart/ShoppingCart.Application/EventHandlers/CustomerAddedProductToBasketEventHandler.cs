609e042f9cd1727d59f4b064b6ad0206df41817a
Frederik Wulf <frederikw@compusoftgroup.com>
2023-03-31 10:56:40 +0200

    add CustomerAddedProductToBasketEventHandler

---
+++ src/Services/ShoppingCart/ShoppingCart.Application/EventHandlers/CustomerAddedProductToBasketEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Common.Kafka;
+using MediatR;
+using ShoppingCart.Core.Events;
+
+namespace ShoppingCart.Application.EventHandlers;
+
+public class CustomerAddedProductToBasketEventHandler : INotificationHandler<CustomerAddedProductToBasketEvent>
+{
+    private readonly KafkaProducer _kafkaProducer;
+    
+    public CustomerAddedProductToBasketEventHandler(KafkaProducer kafkaProducer)
+    {
+        _kafkaProducer = kafkaProducer;
+    }
+    
+    public async Task Handle(CustomerAddedProductToBasketEvent notification, CancellationToken cancellationToken)
+    {
+        _kafkaProducer.Publish("shopping-cart", "customer-added-product-to-basket", notification);
+    }
+}
