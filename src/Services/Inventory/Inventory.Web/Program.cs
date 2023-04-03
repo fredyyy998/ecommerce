3ccc49647b69e683c4a893a9edf1d30153851988
Frederik Wulf <frederikw@compusoftgroup.com>
2023-04-03 13:22:55 +0200

    add docker setup for inventory service

--- src/Services/Inventory/Inventory.Web/Program.cs
+++ src/Services/Inventory/Inventory.Web/Program.cs
@@ -71,11 +71,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 {
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseCors("corsapp");
     app.UseExceptionHandler("/error");
