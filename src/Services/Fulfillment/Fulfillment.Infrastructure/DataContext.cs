d14e24f52faea462752c499df422a5180de3da00
Frederik Wulf <frede.w719@gmail.com>
2023-04-07 11:47:35 +0200

    add DataContext

---
+++ src/Services/Fulfillment/Fulfillment.Infrastructure/DataContext.cs
@@ -0,0 +1,39 @@
+﻿using Fulfillment.Core.Buyer;
+using Fulfillment.Core.Order;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fulfillment.Infrastructure;
+
+public class DataContext : DbContext
+{
+    public DbSet<Buyer> Buyers { get; set; }
+    
+    public DbSet<Order> Orders { get; set; }
+    
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        // necessary to save DateTime
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Buyer>(entity =>
+        {
+            entity.OwnsOne(b => b.PersonalInformation);
+            entity.OwnsOne(b => b.ShippingAddress);
+            entity.OwnsOne(b => b.PaymentInformation);
+        });
+        
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.OwnsOne(o => o.TotalPrice);
+            entity.OwnsMany(o => o.Products, p =>
+            {
+                p.WithOwner().HasForeignKey("OrderId");
+                p.Property<int>("Id").UseIdentityColumn();
+                p.HasKey("Id");
+            });
+        });
+    }
+}
