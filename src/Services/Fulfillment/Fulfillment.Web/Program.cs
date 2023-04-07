091413e6306fcacea60e7470dc0fd00b47a9e398
Frederik Wulf <frede.w719@gmail.com>
2023-04-07 11:59:56 +0200

    add swagger configuration

--- src/Services/Fulfillment/Fulfillment.Web/Program.cs
+++ src/Services/Fulfillment/Fulfillment.Web/Program.cs
@@ -1,6 +1,8 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -10,7 +12,18 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddHttpContextAccessor();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme()
+        {
+            Description = "Standard authorization header using the Bearer scheme (\"bearer {token}\"",
+            In = ParameterLocation.Header,
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey
+        });
 
+        options.OperationFilter<SecurityRequirementsOperationFilter>();
+    });
     
     builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
     {
@@ -30,6 +43,8 @@ var app = builder.Build();
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
 
     app.UseCors("corsapp");
