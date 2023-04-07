6e65165764963e11ee15869578f546d76857a9b5
Frederik Wulf <frede.w719@gmail.com>
2023-04-07 11:56:18 +0200

    add basic web service startup

--- src/Services/Fulfillment/Fulfillment.Web/Program.cs
+++ src/Services/Fulfillment/Fulfillment.Web/Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+{
+    ConfigurationManager configuration = builder.Configuration;
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddHttpContextAccessor();
+
+}
+
 var app = builder.Build();
+{
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+    }
 
-app.MapGet("/", () => "Hello World!");
+    app.UseCors("corsapp");
+    app.UseExceptionHandler("/error");
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    
+    app.Run();
+}
 
-app.Run();
+public partial class Program { }
