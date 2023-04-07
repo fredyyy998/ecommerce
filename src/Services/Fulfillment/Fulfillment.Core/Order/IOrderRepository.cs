a7dcaaf85dae6684be8f430da4056fdd39b651b2
Frederik Wulf <frede.w719@gmail.com>
2023-04-07 10:28:20 +0200

    add IOrderRepository

---
+++ src/Services/Fulfillment/Fulfillment.Core/Order/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Fulfillment.Core.Order;
+
+public interface IOrderRepository
+{
+    Task<Order> FindByIdAsync(Guid orderId);
+    Task<IEnumerable<Order>> FindAllByBuyerIdAsync(Guid buyerId);
+    Task SaveAsync(Order order);
+    Task UpdateAsync(Order order);
+}
