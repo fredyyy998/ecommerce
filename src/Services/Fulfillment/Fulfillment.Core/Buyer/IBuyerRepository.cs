09f398c96301e1291f7a9f6ab764d96c73cd888c
Frederik Wulf <frede.w719@gmail.com>
2023-04-07 10:32:16 +0200

    add IBuyerRepository

---
+++ src/Services/Fulfillment/Fulfillment.Core/Buyer/IBuyerRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Fulfillment.Core.Buyer;
+
+public interface IBuyerRepository
+{
+    Task<Buyer> FindByIdAsync(Guid buyerId);
+    Task SaveAsync(Buyer buyer);
+    Task UpdateAsync(Buyer buyer);
+    Task DeleteAsync(Buyer buyer);
+}
