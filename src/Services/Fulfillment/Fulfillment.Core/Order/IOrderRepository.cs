8725bff6c5f1a975a2583327d76a22c274ef0790
Frederik Wulf <frede.w719@gmail.com>
2023-04-07 11:54:04 +0200

    add OrderRepository implementation

--- src/Services/Fulfillment/Fulfillment.Core/Order/IOrderRepository.cs
+++ src/Services/Fulfillment/Fulfillment.Core/Order/IOrderRepository.cs
@@ -3,7 +3,7 @@
 public interface IOrderRepository
 {
     Task<Order> FindByIdAsync(Guid orderId);
-    Task<IEnumerable<Order>> FindAllByBuyerIdAsync(Guid buyerId);
+    Task<List<Order>> FindAllByBuyerIdAsync(Guid buyerId);
     Task SaveAsync(Order order);
     Task UpdateAsync(Order order);
 }
---
+++ src/Services/Fulfillment/Fulfillment.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using Fulfillment.Core.Order;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fulfillment.Infrastructure.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly DataContext _dataContext;
+    
+    public OrderRepository (DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+    
+    public Task<Order> FindByIdAsync(Guid orderId)
+    {
+        return _dataContext.Orders.FindAsync(orderId).AsTask();
+    }
+
+    public Task<List<Order>> FindAllByBuyerIdAsync(Guid buyerId)
+    {
+        return _dataContext.Orders.Where(o => o.BuyerId == buyerId).ToListAsync();
+    }
+
+    public Task SaveAsync(Order order)
+    {
+        _dataContext.Orders.Add(order);
+        return _dataContext.SaveChangesAsync();
+    }
+
+    public Task UpdateAsync(Order order)
+    {
+        _dataContext.Orders.Update(order);
+        return _dataContext.SaveChangesAsync();
+    }
+}
