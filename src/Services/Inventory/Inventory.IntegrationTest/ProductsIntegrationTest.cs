ab91b6d9265d4e1412775066796a1855f6f80e1d
Frederik Wulf <frede.w719@gmail.com>
2023-04-10 12:07:33 +0200

    add test search products endpoint

---
+++ src/Services/Inventory/Inventory.IntegrationTest/ProductsIntegrationTest.cs
@@ -0,0 +1,60 @@
+using System.Net;
+using System.Net.Http.Json;
+using Inventory.Application.Dtos;
+using Inventory.Core.Product;
+using Inventory.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Inventory.IntegrationTest;
+
+[Collection("Sequential")]
+public class ProductsIntegrationTest : IClassFixture<CustomWebApplicationFactory<Program>>, IDisposable
+{
+    private readonly CustomWebApplicationFactory<Program> _factory;
+
+    public ProductsIntegrationTest(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<DataContext>();
+            
+            var product1 = Product.Create("testProduct", "testDescription", 10.0m);
+            var product2 = Product.Create("otherProduct", "testDescription", 20.0m);
+            var product3 = Product.Create("notStockproduct", "testDescription", 20.0m);
+            product1.AddStock(10);
+            product2.AddStock(20);
+            
+            db.Products.Add(product1);
+            db.Products.Add(product2);
+            db.Products.Add(product3);
+            db.SaveChanges();
+        }
+    }
+    
+    public void Dispose()
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<DataContext>();
+
+            db.Products.RemoveRange(db.Products);
+            db.SaveChanges();
+        }
+    }
+    [Fact]
+    public async Task Products_In_Sock_Are_Returned_As_List()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        // Act
+        var response = await client.GetAsync($"/api/Products");
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var products = await response.Content.ReadFromJsonAsync<List<ProductResponseDto>>();
+        Assert.Equal(2, products.Count);
+    }
+}
