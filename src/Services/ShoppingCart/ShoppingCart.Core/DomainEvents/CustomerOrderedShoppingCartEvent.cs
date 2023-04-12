96c823f8c9a1c1de4e4a4bb4e3e78a92b26ba126
Frederik Wulf <frede.w719@gmail.com>
2023-04-12 13:04:19 +0200

    refactoring: flattened CustomerOrderedShoppingCartEvent to be easier to deserialize

--- src/Services/ShoppingCart/ShoppingCart.Core/DomainEvents/CustomerOrderedShoppingCartEvent.cs
+++ src/Services/ShoppingCart/ShoppingCart.Core/DomainEvents/CustomerOrderedShoppingCartEvent.cs
@@ -1,13 +1,28 @@
 ﻿using Ecommerce.Common.Core;
+using ShoppingCart.Core.ShoppingCart;
 
 namespace ShoppingCart.Core.Events;
 
 public class CustomerOrderedShoppingCartEvent : IDomainEvent
 {
-    public ShoppingCart.ShoppingCart ShoppingCart { get; }
+    public Guid ShoppingCartId { get; }
     
+    public Guid CustomerId { get; }
+    
+    private List<ShoppingCartItem> _items;
+
+    public IReadOnlyCollection<ShoppingCartItem> Items => _items.AsReadOnly();
+    
+    public DateTime CreatedAt { get; }
+
+    public DateTime? UpdatedAt { get; }
+
     public CustomerOrderedShoppingCartEvent(ShoppingCart.ShoppingCart shoppingCart)
     {
-        ShoppingCart = shoppingCart;
+        ShoppingCartId = shoppingCart.Id;
+        CustomerId = shoppingCart.CustomerId;
+        _items = shoppingCart.Items.ToList();
+        CreatedAt = shoppingCart.CreatedAt;
+        UpdatedAt = shoppingCart.UpdatedAt;
     }
 }
