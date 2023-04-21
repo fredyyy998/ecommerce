9e8b0932eea5d283b5265c330def3abf14a6ea10
Frederik Wulf <frede.w719@gmail.com>
2023-04-21 16:51:49 +0200

    feature: add revenue controller

---
+++ src/Services/Fulfillment/Fulfillment.Web/Controller/RevenuesController.cs
@@ -0,0 +1,33 @@
+﻿using Fulfillment.Application.Dtos;
+using Fulfillment.Application.Services.Revenue;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fulfillment.Web;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public class RevenuesController : BaseController
+{
+    private readonly IRevenueService _revenueService;
+    
+    public RevenuesController(IRevenueService revenueService)
+    {
+        _revenueService = revenueService;
+    }
+    
+    [HttpGet()]
+    public async Task<IActionResult> GetRevenues([FromQuery] RevenueQuery query)
+    {
+        try
+        {
+            var revenues = await _revenueService.GetRevenueReportAsync(query);
+            return Ok(revenues);
+        }
+        catch (Exception e)
+        {
+            return HandleException(e);
+        }
+
+    }
+}
