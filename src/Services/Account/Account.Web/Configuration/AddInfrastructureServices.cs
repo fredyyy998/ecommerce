10b71492bb3058e3abb123c2a5cb49172954c9e0
Frederik Wulf <frede.w719@gmail.com>
2023-04-25 21:42:13 +0200

    feature: add IServiceInstaller abstraction to split configuration

--- src/Services/Account/Account.Web/Configuration/AddInfrastructureServices.cs
+++ src/Services/Account/Account.Web/Configuration/AddInfrastructureServices.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System.Reflection;
+using Account.Core.Administrator;
+using Account.Core.User;
+using Account.Infrastructure;
+using Account.Infrastructure.Repository;
+using Ecommerce.Common.Web;
+using Microsoft.EntityFrameworkCore;
+
+public class InfrascructureInstaller : IServiceInstaller
+{
+    
+    
+    public void InstallService(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<DataContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly("Account.Web")));
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IAdministratorRepository, AdministratorRepository>();
+    }
+}
--- src/Services/Account/Account.Web/Program.cs
+++ src/Services/Account/Account.Web/Program.cs
@@ -1,92 +1,12 @@
 using System.Reflection;
-using System.Text;
-using Account.Application;
-using Account.Application.Dtos;
-using Account.Application.EventHandler;
-using Account.Application.Profile;
-using Account.Core.Administrator;
-using Account.Core.Events;
-using Account.Core.User;
-using Account.Infrastructure;
-using Account.Infrastructure.Repository;
-using Ecommerce.Common.Kafka;
-using FluentValidation;
-using MediatR;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Filters;
+using Ecommerce.Common.Web;
 
 var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 {
     ConfigurationManager configuration = builder.Configuration;
-    // Add services to the container.
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddHttpContextAccessor();
-    builder.Services.AddSwaggerGen();
-    builder.Services.AddSwaggerGen(options =>
-    {
-        options.SwaggerDoc("v1", new OpenApiInfo
-        {
-            Title = "Account Service API", Version = "v1",
-            Description = "The account service is a microservice of the Ecommerce application (https://github.com/fredyyy998/ecommerce). It is responsible for managing the user accounts of the application.",
-            
-        });
-        
-        
-        options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme()
-        {
-            Description = "Standard authorization header using the Bearer scheme (\"bearer {token}\"",
-            In = ParameterLocation.Header,
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey
-        });
 
-        options.OperationFilter<SecurityRequirementsOperationFilter>();
-        
-        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
-    });
-
-    builder.Services.AddScoped<IValidator<CustomerCreateDto>, CustomerCreateDtoValidator>();
-    builder.Services.AddAutoMapper(typeof(MappingProfile));
-    builder.Services.AddDbContext<DataContext>(options =>
-        options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
-            b => b.MigrationsAssembly("Account.Web")));
-    
-
-    var jwtInformation = new JwtInformation(configuration["JWT:Secret"], configuration["JWT:ValidIssuer"], configuration["JWT:ValidAudience"]);
-    builder.Services.AddSingleton<JwtInformation>(jwtInformation);
-    
-    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters()
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey =
-                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT:Secret").Value)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-    
-    builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-    builder.Services.AddScoped<IAdministratorRepository, AdministratorRepository>();
-
-    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-    builder.Services.AddScoped<IProfileService, ProfileService>();
-
-    builder.Services.AddSingleton<KafkaProducer>(new KafkaProducer(configuration["Kafka:BootstrapServers"], configuration["Kafka:ClientId"]));
-    builder.Services.AddScoped<INotificationHandler<CustomerRegisteredEvent>, CustomerRegisteredEventHandler>();
-    builder.Services.AddScoped<INotificationHandler<CustomerEditedEvent>, CustomerEditedEventHandler>();
-    
-    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-    
-    
     builder.Services.AddCors(options =>
     {
         options.AddPolicy(name: MyAllowSpecificOrigins,
@@ -97,6 +17,8 @@ var builder = WebApplication.CreateBuilder(args);
                     .AllowAnyMethod();
             });
     });
+
+    builder.Services.InstallServices(builder.Configuration, Assembly.GetExecutingAssembly());
 }
 
 var app = builder.Build();
