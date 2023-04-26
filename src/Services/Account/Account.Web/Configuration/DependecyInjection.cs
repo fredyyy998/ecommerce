9011e308c83374a733bfea3367773b05f8dd3a05
Frederik Wulf <frede.w719@gmail.com>
2023-04-26 13:45:02 +0200

    bugfix: adjusted dpeendency injection to use the nuget dependency injection

--- src/Services/Account/Account.Web/Configuration/DependecyInjection.cs
+++ src/Services/Account/Account.Web/Configuration/DependecyInjection.cs
@@ -3,17 +3,13 @@ using Ecommerce.Common.Web;
 
 namespace Account.Web.Configuration;
 
-public static class DependencyInjection
+public static class DependencyInjectionImplementation
 {
     public static IServiceCollection InstallServicesTest(
         this IServiceCollection services,
         IConfiguration configuration,
         params Assembly[] assemblies)
     {
-        foreach (IServiceInstaller serviceInstaller in ((IEnumerable<Assembly>) assemblies).SelectMany<Assembly, TypeInfo>((Func<Assembly, IEnumerable<TypeInfo>>) (x => x.DefinedTypes)).Where<TypeInfo>(new Func<TypeInfo, bool>(IsAssignableType<IServiceInstaller>)).Select<TypeInfo, object>(new Func<TypeInfo, object>(Activator.CreateInstance)).Cast<IServiceInstaller>())
-            serviceInstaller.InstallService(services, configuration);
-        return services;
-
-        static bool IsAssignableType<T>(TypeInfo typeInfo) where T : notnull => typeof (T).IsAssignableFrom((Type) typeInfo) && !typeInfo.IsInterface && !typeInfo.IsAbstract;
+        return DependencyInjection.InstallServices(services, configuration, assemblies);
     }
 }
