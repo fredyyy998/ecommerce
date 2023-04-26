b18f77452e59d0311acbbaac76796b60a35f1684
Frederik Wulf <frede.w719@gmail.com>
2023-04-26 19:58:56 +0200

    feature: add DependencyInjection.cs to common web package

---
+++ src/Ecommerce.Common/Ecommerce.Common.Web/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Common.Web;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection InstallServices(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        params Assembly[] assemblies)
+    {
+
+        IEnumerable<IServiceInstaller> serviceInstallers = assemblies
+            .SelectMany(assembly => assembly.DefinedTypes)
+            .Where(IsAssignableType<IServiceInstaller>)
+            .Select(Activator.CreateInstance)
+            .Cast<IServiceInstaller>();
+
+        foreach (var serviceInstaller in serviceInstallers)
+        {
+            serviceInstaller.InstallService(services, configuration);
+        }
+
+        return services;
+        
+        static bool IsAssignableType<T>(TypeInfo typeInfo) where T : notnull => typeof (T).IsAssignableFrom((Type) typeInfo) && !typeInfo.IsInterface && !typeInfo.IsAbstract;
+    }
+}
