f96cc9ebb760864acfac67d8e702de3941e34b81
Frederik Wulf <frede.w719@gmail.com>
2023-04-27 14:35:33 +0200

    feature: add endpoint to change state to delivered

--- src/Services/Fulfillment/Fulfillment.Web/Controller/OrderManagementController.cs
+++ src/Services/Fulfillment/Fulfillment.Web/Controller/OrderManagementController.cs
@@ -50,6 +50,35 @@ public class OrderManagementController : BaseController
     }
     
     /// <summary>
+    /// Changes the state of an order to Delivered
+    /// Only usable by Admins
+    /// </summary>
+    /// <param name="orderId">The id of the order</param>
+    /// <returns>nothing</returns>
+    /// <response code="204">If the order state was changed to delivered successfully</response>
+    /// <response code="401">If the user is not authorized</response>
+    /// <response code="403">If the user is not an admin</response>
+    /// <response code="404">If the order does not exist</response>
+    /// <response code="500">If an internal server error occurs</response>
+    [HttpPut("{orderId}/state/deliver")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeliverOrder(Guid orderId)
+    {
+        try
+        {
+            await _orderService.DeliverOrder(orderId);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return HandleException(e);
+        }
+    }
+    
+    /// <summary>
     /// Returns all orders in a paginated list
     /// </summary>
     /// <param name="parameters">The pagination and filter parameters</param>
