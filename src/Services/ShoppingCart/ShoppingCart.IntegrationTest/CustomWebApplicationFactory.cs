06d2b8d617c700811cf00dd195d9b1d789a5eb14
fredyyy998 <frede.w719@gmail.com>
2023-05-03 20:57:16 +0200

    testing: add integration test setup for shopping cart

---
+++ src/Services/ShoppingCart/ShoppingCart.IntegrationTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingCart.Infrastructure;
+
+namespace Account.IntegrationTest;
+
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            context.HostingEnvironment.ContentRootPath = Path.Combine(Directory.GetCurrentDirectory());
+
+            config.AddJsonFile("appsettings.IntegrationTest.json");
+        });
+        
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<DataContext>));
+
+            services.Remove(dbContextDescriptor);
+
+            var configuration = services.BuildServiceProvider().GetService<IConfiguration>();
+
+            services.AddDbContext<DataContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("ShoppingCart.Web")));
+        });
+
+        builder.UseEnvironment("Development");
+
+    }
+}
