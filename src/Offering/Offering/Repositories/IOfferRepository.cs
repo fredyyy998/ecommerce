33b41a3ce71ecc60900591447c3072fa87128ffd
Frederik Wulf <frede.w719@gmail.com>
2023-06-14 11:16:54 +0200

    add aggregate root repository interfaces

---
+++ src/Offering/Offering/Repositories/IOfferRepository.cs
@@ -0,0 +1,22 @@
+﻿using Offering.Models;
+
+namespace Offering.Repositories;
+
+public interface IOfferRepository
+{
+    Task<List<Offer>> FindAll(); 
+    
+    Task<List<Offer>> FindAllAvailable();
+
+    Task<List<Offer>> FindAllExpired();
+    
+    Task<Offer> FindById(Guid id);
+    
+    Task<List<Offer>> findByProduct(Guid productId);
+    
+    Task Add(Offer offer);
+    
+    Task Update(Offer offer);
+    
+    Task Delete(Offer offer);
+}
---
+++ src/Offering/Offering/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Offering.Models;
+
+namespace Offering.Repositories;
+
+public interface IProductRepository
+{
+    Task<Product> FindById(Guid id);
+    
+    Task<List<Product>> FindAll();
+    
+    Task Add(Product product);
+    
+    Task Update(Product product);
+    
+    Task Delete(Product product);
+}
