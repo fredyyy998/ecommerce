beef780753075eb3eaaecb82fa7d53b9663f9e31
Frederik Wulf <frede.w719@gmail.com>
2023-06-16 15:38:22 +0200

    demo test setup with production database

---
+++ src/Offering/OfferingTest/OfferControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Offering.Controllers;
+using Offering.Dtos;
+using Offering.Models;
+using Offering.Repositories;
+
+namespace OfferingTest;
+
+public class OfferControllerTest : IClassFixture<TestDatabaseFixture>, IDisposable
+{
+    public TestDatabaseFixture Fixture { get; }
+
+    public OfferControllerTest(TestDatabaseFixture fixture)
+    {
+        Fixture = fixture;
+    }
+
+    public void Dispose()
+    {
+        Fixture.Cleanup();
+    }
+
+    [Fact]
+    public async void ListOffers_Should_Return_All_Offers()
+    {
+        // Arrange
+        var context = Fixture.CreateContext();
+        var _offerRepository = new OfferRepository(context);
+        var _productRepository = new ProductRepository(context);
+        var controller = new OfferController(_offerRepository, _productRepository);
+        
+        // Act
+        var result = await controller.ListOffers();
+        
+        // Assert
+        var okResult = result as OkObjectResult;
+        var value = okResult.Value as List<Offer>;
+        Assert.Equal(1, value.Count);
+    }
+
+    [Fact]
+    public async void CreateSingleOffer_Should_Be_Persisted()
+    {
+        // Arrange
+        var context = Fixture.CreateContext();
+        var _offerRepository = new OfferRepository(context);
+        var _productRepository = new ProductRepository(context);
+        var controller = new OfferController(_offerRepository, _productRepository);
+        var product = Product.Create(Guid.NewGuid(), "Product 2", "Description");
+        await _productRepository.Add(product);
+        var createSingleOfferRequestDto = new CreateSingleOfferRequestDto("Offer 2", 10, 19, "EUR", DateTime.Now, DateTime.Now.AddDays(1), product.Id);
+
+        // Act
+        var result = await controller.CreateSingleOffer(createSingleOfferRequestDto);
+        
+        // Assert
+        var okResult = result as OkObjectResult;
+        var value = okResult.Value as SingleOffer;
+        Assert.Equal("Offer 2", value.Name);
+        var offer = context.Offers.Single(o => o.Name == "Offer 2");
+        Assert.Equal(offer.Name, value.Name);
+    }
+    
+}
---
+++ src/Offering/OfferingTest/TestDatabaseFixture.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Offering.Models;
+using Offering.Repositories;
+
+namespace OfferingTest;
+
+public class TestDatabaseFixture
+{
+    private const string ConnectionString = "server=localhost;database=db;user=offer-user;password=password";
+
+    public TestDatabaseFixture()
+    {
+        using var context = CreateContext();
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        Cleanup();
+    }
+
+    public void Cleanup()
+    {
+        var context = CreateContext();
+        
+        context.RemoveRange(context.Offers);
+        context.RemoveRange(context.Products);
+
+        var price = Price.CreateFromGross(10, 19, "EUR");
+        var product1 = Product.Create(Guid.NewGuid(), "Product 1", "Description");
+        var product2 = Product.Create(Guid.NewGuid(), "Product 2", "Description");
+        var offer = SingleOffer.Create("Offer 1", price, DateTime.Now, DateTime.Now.AddDays(1), product1);
+        context.AddRange(
+            product1,
+            product2,
+            offer
+        );
+        context.SaveChanges();
+        
+    }
+    
+    public DataContext CreateContext()
+        => new DataContext(
+            new DbContextOptionsBuilder<DataContext>()
+                .UseMySQL(ConnectionString)
+                .Options);
+}
